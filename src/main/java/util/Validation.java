a60ce5305789a6f1fb9a3fcb0882deadc3f74179
saranya-3012 <saranyar091103@gmail.com>
2026-02-17 15:51:28 +0530

    Update Validation.java

--- src/main/java/util/Validation.java
+++ src/main/java/util/Validation.java
@@ -1,44 +1,54 @@
 package util;
 
+import java.util.regex.Pattern;
+
 public class Validation {
+	
+	   // Name Pattern
+	   private static final String USERNAME = "^[a-zA-Z0-9_]{3,20}$";
+	
+    	// Email Pattern
+    	private static final String EMAIL = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
 
-    // Name Validation
-    public static boolean ValidName(String name) {
-        if(name == null || name.isEmpty()) {
-            return false;
-        }
-        return true;
-    }
+	    // Indian phone number (10 digits)
+	    // Start with 6-9, Numbers between 0-9, After 1st have 9 digits
+	    private static final String PHONE = "^[6-9][0-9]{9}$";
 
-    // Email Validation
-    public static boolean ValidEmail(String email) {
-        if(email == null || !email.contains("@")) {
-            return false;
-        }
-        return true;
-    }
+	    // Password rules:
+	    // Min 8 chars, 1 uppercase, 1 lowercase, 1 number, 1 special char
+	    private static final String PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
 
-    // Amount Validation
-    public static boolean ValidAmount(double amt) {
-        if(amt <= 0) {
-            return false;
-        }
-        return true;
-    }
+	    // Amount Pattern
+		private static final String AMOUNT = "^\\d+(\\.\\d{1,2})?$";
+		
+		
+		
+		
+	    // Name validation
+		public static boolean isValidUsername(String username) {
+			return username != null && Pattern.matches(USERNAME, username);
+		}
+	    
+	    // Email Validation
+	    public static boolean isValidEmail(String email) {
+	        return email != null && Pattern.matches(EMAIL, email);
+	    }
+   
 
-    // Password Validation
-    public static boolean ValidPass(String pass) {
-        if(pass == null || pass.length() < 8) {
-            return false;
-        }
-        return true;
-    }
+	    // Phone Validation
+	    public static boolean isValidPhone(String phone) {
+	        return phone != null && Pattern.matches(PHONE, phone);
+	    }
 
-    // Role Validation
-    public static boolean ValidRole(String role) {
-        if(role == null || role.isEmpty()) {
-            return false;
-        }
-        return true;
-    }
-}
+	    // Password Validation
+	    public static boolean isValidPassword(String password) {
+	        return password != null && Pattern.matches(PASSWORD, password);
+	    }
+	    
+     	 // Amount Validation
+	    public static boolean isValidAmount(String amount) {
+	    	return amount != null && Pattern.matches(AMOUNT, amount);
+	    }
+	}
+
+   
