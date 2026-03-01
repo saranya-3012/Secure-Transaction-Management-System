98f6b9d81bc6e99fafb1293775183190ad3b5ca4
saranya-3012 <saranyar091103@gmail.com>
2026-03-01 11:04:38 +0530

    Update AccountDAO.java

--- src/main/java/dao/AccountDAO.java
+++ src/main/java/dao/AccountDAO.java
@@ -1,130 +1,53 @@
 package dao;
 
-import java.math.BigDecimal;
-import java.sql.*;
+import com.digital.model.Account;
+import com.digital.util.DBConnection;
 
-import model.Account;
-import util.AccountNoGenerator;
-import util.DBConnection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountDAO {
-	
-	// Create New Account
-	public String createAccount(Account account) throws SQLException {
-		
-	   String generatedAccountNo = AccountNoGenerator.generateAccountNumber();
-	   
-       String sql = "INSERT INTO Account(Customer_ID, Account_No, Balance) VALUES(?,?,?)";
 
-       try (Connection con = DBConnection.getConnection();
-           PreparedStatement ps = con.prepareStatement(sql)) {
+	// Create a New account
+	public void create(Account account) throws Exception {
+		String sql = "INSERT INTO Accounts(Customer_id,Account_number,Balance,Account_type) VALUES(?,?,?,?)";
 
-           ps.setInt(1, account.getcust_id());
-           ps.setString(2, generatedAccountNo);
-           ps.setBigDecimal(3, account.getbalance());
+		try (Connection con = DBConnection.getConnection();
+			 PreparedStatement ps = con.prepareStatement(sql)) {
 
-           ps.executeUpdate();
-           
-           return "Account Created Successfully!";
-       }
-       catch (Exception e) {
-    	  e.printStackTrace();
-       }
-       return null;
+			ps.setInt(1, account.getCustomerId());
+			ps.setString(2, account.getAccountNumber());
+			ps.setBigDecimal(3, account.getBalance());
+			ps.setString(4, account.getAccountType());
+
+			ps.executeUpdate();
+		}
 	}
-	
-	
-	
-  // Get Account Details by Account Number
-	public Account getAccountByAccNo(String acc_no) throws Exception {
 
-		String sql = "SELECT Account_ID, Account_No, Balance FROM Account WHERE Account_No = ?";
+	// View Account Details by Customer ID
+	public List<Account> findByCustomerId(int customerId) throws Exception {
 
-		try (Connection con = DBConnection.getConnection(); 
-			PreparedStatement ps = con.prepareStatement(sql)) {
+		List<Account> list = new ArrayList<>();
+		String sql = "SELECT * FROM accounts WHERE customer_id=?";
 
-			ps.setString(1, acc_no);
+		try (Connection con = DBConnection.getConnection();
+			 PreparedStatement ps = con.prepareStatement(sql)) {
+
+			ps.setInt(1, customerId);
 			ResultSet rs = ps.executeQuery();
 
-			if (rs.next()) {
-				Account account = new Account();
-				account.setacc_id(rs.getLong("Account_ID"));
-				account.setacc_no(rs.getString("Account_No"));
-				account.setbalance(rs.getBigDecimal("Balance"));
-				return account;
+			while (rs.next()) {
+				list.add(new Account(
+						rs.getInt("account_id"),
+						rs.getInt("customer_id"),
+						rs.getString("account_number"),
+						rs.getBigDecimal("balance"),
+						rs.getString("account_type"),
+						rs.getTimestamp("created_at").toLocalDateTime()
+				));
 			}
-		} 
-		catch (Exception e) {
-			throw new Exception("Failed to get account Number", e);
 		}
-
-		return null;
+		return list;
 	}
-	
-	
-	
-	 // Find Balance by Account Number
-		public BigDecimal getBalance(String acc_no) throws Exception {
-
-			String sql = "SELECT Balance FROM Account WHERE Account_No = ?";
-
-			try (Connection con = DBConnection.getConnection(); 
-				PreparedStatement ps = con.prepareStatement(sql)) {
-
-				ps.setString(1, acc_no);
-				ResultSet rs = ps.executeQuery();
-
-				if (rs.next()) {					
-					return(rs.getBigDecimal("Balance"));
-				}
-			} 
-			catch (Exception e) {
-				throw new Exception("Failed to get Account Number", e);
-			}
-
-			return null;
-		}
-	
-		
-		
-  // Deposit Amount
-	public void deposit(String account_no, BigDecimal amount) throws Exception {
-
-		AccountDAO accdao = new AccountDAO();
-		BigDecimal pre_amount = accdao.getBalance(account_no);
-		BigDecimal total_amount = pre_amount.add(amount);
-		
-	    String sql = "UPDATE Account SET Balance = ? WHERE Account_No = ?";
-
-	    try (Connection con = DBConnection.getConnection();
-	         PreparedStatement ps = con.prepareStatement(sql)) {
-
-	        ps.setBigDecimal(1, total_amount);
-	        ps.setString(2, account_no);
-
-	        ps.executeUpdate();
-	    }
-	}
-	
-	
-	
- // Withdrawal Amount
-		public void withdrwal(String account_no, BigDecimal amount) throws Exception {
-
-			AccountDAO accdao = new AccountDAO();
-			BigDecimal pre_amount = accdao.getBalance(account_no);
-			BigDecimal total_amount = pre_amount.subtract(amount);
-			
-		    String sql = "UPDATE Account SET Balance = ? WHERE Account_No = ?";
-
-		    try (Connection con = DBConnection.getConnection();
-		         PreparedStatement ps = con.prepareStatement(sql)) {
-
-		        ps.setBigDecimal(1, total_amount);
-		        ps.setString(2, account_no);
-
-		        ps.executeUpdate();
-		    }
-		}
-
 }
