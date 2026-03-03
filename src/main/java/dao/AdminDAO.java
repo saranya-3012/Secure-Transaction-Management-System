c98ab89d216e9dc78d9fc0411367cc04b497bbc9
saranya-3012 <saranyar091103@gmail.com>
2026-03-03 16:14:31 +0530

    Update AdminDAO.java

--- src/main/java/dao/AdminDAO.java
+++ src/main/java/dao/AdminDAO.java
@@ -1,16 +1,19 @@
 package dao;
 
 import model.Admin;
+import model.Customer;
 import util.DBConnection;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Optional;
 
 public class AdminDAO {
 
-    // View Admin Details
+    // View User Details
     public Optional<Admin> findByUsername(String username) throws Exception {
-        String sql = "SELECT * FROM Admin WHERE username=?";
+        String sql = "SELECT * FROM Admin WHERE Username=?";
 
         try (Connection con = DBConnection.getConnection();
              PreparedStatement ps = con.prepareStatement(sql)) {
@@ -19,13 +22,12 @@ public class AdminDAO {
             ResultSet rs = ps.executeQuery();
 
             if (rs.next()) {
-                Admin admin = new Admin();
-                admin.setAdminId(rs.getInt("admin_id"));
-                admin.setUsername(rs.getString("username"));
-                admin.setPassword(rs.getString("password"));
-                admin.setCreatedAt(rs.getTimestamp("created_at").toLocalDateTime()
-                );
-
+                        Admin admin = new Admin();
+                        admin.setAdminId(rs.getInt("admin_id"));
+                        admin.setUsername(rs.getString("username"));
+                        admin.setPassword(rs.getString("password"));
+                        admin.setCreatedAt(rs.getTimestamp("created_at").toLocalDateTime()
+                        );
                 return Optional.of(admin);
             }
         }
