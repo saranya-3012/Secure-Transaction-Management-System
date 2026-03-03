ed8c62a9498c4d7a8f9b0050b7388fbef11bd1a2
saranya-3012 <saranyar091103@gmail.com>
2026-03-03 16:18:16 +0530

    Update AccountNoGenerator.java

--- src/main/java/util/AccountNoGenerator.java
+++ src/main/java/util/AccountNoGenerator.java
@@ -3,25 +3,22 @@ package util;
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.concurrent.ThreadLocalRandom;
 
 public final class AccountNoGenerator {
-	
+
 	private static final SecureRandom SECURE_RANDOM = new SecureRandom();
-	private static final int RANDOM_SUFFIX=100;
 
-
-    private AccountNoGenerator() {
-	
-	   throw new UnsupportedOperationException("Utility class");
-    }
-
-	public static String generateAccountNumber() {
-		
-		int randomSuffix = SECURE_RANDOM.nextInt(RANDOM_SUFFIX);
-		
-		return String.format("%02d", randomSuffix);
-
+	private AccountNoGenerator() {
+		throw new UnsupportedOperationException("Utility class");
 	}
 
+	public static String generateAccountNumber() {
+
+		String timePart = LocalDateTime.now()
+				.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+
+		int randomPart = 1000 + SECURE_RANDOM.nextInt(9000);
+
+		return "ACC" + timePart + randomPart;
+	}
 }
