5558cfdcf57d2cbb2f5fd17e4114931ae80c28d1
saranya-3012 <saranyar091103@gmail.com>
2026-03-08 23:26:21 +0530

    Change register method parameters

--- src/main/java/dao/CustDAO.java
+++ src/main/java/dao/CustDAO.java
@@ -9,17 +9,18 @@ import java.util.Optional;
 public class CustDAO {
 
     // Registration
-    public void register(Customer customer) throws Exception {
-        String sql = "INSERT INTO Customer(Username, Password, Full_name, Email, Phone) VALUES(?,?,?,?,?)";
+    public void register(String username, String password, String name, String email, String phone) throws Exception {
+        String sql = "INSERT INTO Customer(Username, Password, Full_name, Email, Phone, Role) VALUES(?,?,?,?,?,?)";
 
         try (Connection con = DBConnection.getConnection();
              PreparedStatement ps = con.prepareStatement(sql)) {
 
-            ps.setString(1, customer.getUsername());
-            ps.setString(2, customer.getPassword());
-            ps.setString(3, customer.getFullName());
-            ps.setString(3, customer.getPhone());
-            ps.setString(4, customer.getEmail());
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ps.setString(3, name);
+            ps.setString(4, email);
+            ps.setString(5, phone);
+            ps.setString(6, "Customer");
 
             ps.executeUpdate();
         }
@@ -37,17 +38,14 @@ public class CustDAO {
 
             if (rs.next()) {
                         Customer customer = new Customer();
-                        customer.setCustomerId(rs.getInt("customer_id"));
                         customer.setUsername(rs.getString("username"));
                         customer.setPassword(rs.getString("password"));
                         customer.setFullName(rs.getString("full_name"));
                         customer.setEmail(rs.getString("email"));
-                        customer.setCreatedAt(rs.getTimestamp("created_at").toLocalDateTime()
-                        );
+
                 return Optional.of(customer);
             }
         }
         return Optional.empty();
     }
 }
-
