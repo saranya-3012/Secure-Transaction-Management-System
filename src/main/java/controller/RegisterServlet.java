3a28cabf54c37b6896956f20a1005b29a2af970c
saranya-3012 <saranyar091103@gmail.com>
2026-03-29 16:37:46 +0530

    Update RegisterServlet.java

--- src/main/java/controller/RegisterServlet.java
+++ src/main/java/controller/RegisterServlet.java
@@ -6,6 +6,7 @@ import util.PasswordHash;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import java.util.logging.Level;
 
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
@@ -30,8 +31,7 @@ public class RegisterServlet extends HttpServlet {
             CustomerDAO customerDao = new CustomerDAO();
             customerDao.register(username, hashedPassword, name, email, phone);
             resp.getWriter().println(name + " registered successfully");
-            AppLogger.LOGGER.info(String.format("%s registered successfully", username));
-        }
+            AppLogger.LOGGER.log(Level.INFO, "{0} registered successfully", username);        }
         catch (Exception e) {
             AppLogger.LOGGER.severe(String.format("Error while registering customer: %s ", e.getMessage()));
         }
