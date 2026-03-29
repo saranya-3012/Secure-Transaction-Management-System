58e4b590ec6999087947ae72807dd16f27417eed
saranya-3012 <saranyar091103@gmail.com>
2026-03-29 13:09:41 +0530

    Changed Phone validation

--- src/main/java/util/Validation.java
+++ src/main/java/util/Validation.java
@@ -4,16 +4,20 @@ import java.util.regex.Pattern;
 
 public final class Validation {
 
+	private Validation(){
+		throw new UnsupportedOperationException("Utility class");
+	}
+
 	   private static final String USERNAME = "^[a-z0-9_]{5,15}$";
 
 	   private static final String EMAIL = "^[a-z0-9_.-]+@[a-z.-]+$";
 
-	   private static final String PHONE = "^[6-9][0-9]{9}$";
+	   private static final String PHONE = "^[6-9]\\d{9}$";
 
 	   private static final String PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
 
 		public static boolean isValidUsername(String username) {
-			return username != null && Pattern.matches(USERNAME, username);
+			return username == null || !Pattern.matches(USERNAME, username);
 		}
 
 	    public static boolean isValidEmail(String email) {
@@ -25,7 +29,7 @@ public final class Validation {
 	    }
 
 	    public static boolean isValidPassword(String password) {
-	        return password != null && Pattern.matches(PASSWORD, password);
+	        return password == null || !Pattern.matches(PASSWORD, password);
 	    }
 
 	}  
