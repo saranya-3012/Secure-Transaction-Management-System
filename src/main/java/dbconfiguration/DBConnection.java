b7b1ce0b064f94f7eb7c9bdc8be44c6cb9e3520b
saranya-3012 <saranyar091103@gmail.com>
2026-03-29 11:48:48 +0530

    Added a private constructor with UnsupportedOperationException

--- src/main/java/dbconfiguration/DBConnection.java
+++ src/main/java/dbconfiguration/DBConnection.java
@@ -10,29 +10,22 @@ import java.util.Properties;
 
 public class DBConnection {
 
-    // HikariDataSource - Create and manage the database connection pool
     private static final HikariDataSource dataSource;
 
-    // Automatically executes when the class loads by JVM
+    private AccountNoGenerator() {
+		throw new UnsupportedOperationException("Utility class");
+	}
+    
     static {
         try {
-            // Store configuration data in key-value pairs
             Properties props = new Properties();
 
-            // DBConnection.class  - Gives the class object
-            // getClassLoader() - Find the path of given file
-            // getResourceAsStream() - Open and reads the file data as bytes
-            // InputStream - Reads the existing data inside the file as a byte
             InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("application.properties");
 
-            // Stores the byte input into prop object
-            // load() - Read and converts the byte data into key-value pair data
             props.load(input);
 
-            // Used to configure the connection pool
             HikariConfig config = new HikariConfig();
 
-            // getProperty() - Retrieve the data inside the file using its key
             config.setJdbcUrl(props.getProperty("url"));
             config.setUsername(props.getProperty("username"));
             config.setPassword(props.getProperty("password"));
@@ -43,7 +36,6 @@ public class DBConnection {
             config.setIdleTimeout(Integer.parseInt(props.getProperty("hikari.idleTimeout")));
             config.setConnectionTimeout(Integer.parseInt(props.getProperty("hikari.connectionTimeout")));
 
-            // Gives the stored connection pool data into HikariDataSource
             dataSource = new HikariDataSource(config);
 
         }
