83032077850483f94e2084ea85bbb071bc53601b
saranya-3012 <saranyar091103@gmail.com>
2026-03-29 12:48:15 +0530

    Added a private constructor with UnsupportedOperationException

--- src/main/java/service/TransactionService.java
+++ src/main/java/service/TransactionService.java
@@ -6,6 +6,10 @@ public class TransactionService {
 
 	private static final TransactionDAO transactionDAO = new TransactionDAO();
 
+	private TransactionService(){
+		throw new UnsupportedOperationException("Utility class");
+	}
+
 	public static void transfer(int fromAccount, int toAccount, double amount) throws Exception {
 		transactionDAO.transferMoney(fromAccount, toAccount, amount);
 	}
