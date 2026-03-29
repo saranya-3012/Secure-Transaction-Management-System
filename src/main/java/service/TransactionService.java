4e2e1dd9fefc5edea395d3d0e494040fc9cac494
saranya-3012 <saranyar091103@gmail.com>
2026-03-29 15:32:54 +0530

    Added try/catch block

--- src/main/java/service/TransactionService.java
+++ src/main/java/service/TransactionService.java
@@ -2,15 +2,20 @@ package service;
 
 import dao.TransactionDAO;
 
+import java.sql.SQLException;
+
 public class TransactionService {
 
 	private static final TransactionDAO transactionDAO = new TransactionDAO();
 
-	private TransactionService(){
-		throw new UnsupportedOperationException("Utility class");
-	}
+	private TransactionService() {}
 
-	public static void transfer(int fromAccount, int toAccount, double amount) throws Exception {
-		transactionDAO.transferMoney(fromAccount, toAccount, amount);
+	public static void transfer(int fromAccount, int toAccount, double amount) throws SQLException {
+		try {
+			transactionDAO.transferMoney(fromAccount, toAccount, amount);
+		}
+		catch (SQLException e){
+			throw new SQLException(e);
+		}
 	}
 }
